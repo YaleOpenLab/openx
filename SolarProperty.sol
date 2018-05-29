38e46bfd439377964c70f7acf86a80a865053b33
Michelle Lauer <mflauer@mit.edu>
2018-05-29 10:27:10 -0400

    approver concept, ability to add new ss

--- SolarProperty.sol
+++ SolarProperty.sol
@@ -1,8 +1,9 @@
+pragma solidity ^0.4.0;
 contract SolarProperty {
     
     /* declaration of specialized data types */
     enum HoldingStatus {OWNED, HELD, AVAILABLE}
-    enum PaymentStatus {Paid, Overdue}
+    enum PaymentStatus {PAID, OVERDUE}
     
     struct SolarSystem {
         string name;
@@ -15,30 +16,53 @@ contract SolarProperty {
     }
     
     /* public variables */
+    address approver;
     SolarSystem[] public solarSystems;
 
     /* public event on the blockchain, clients notified */
-    event AddHolder(address holder, uint ssIndex);
+    event AddSolarSystem(string name);
+    event TransferHolder(address holder, uint ssIndex);
     event Payment(address payer, uint unpaidBalance); // if paid in full, unpaidBalance = 0
     event Repo(uint ssIndex, address lateHolder); // reposessing unpaid system, hardware can listen for this
 
     /* runs at initialization when contract is executed */
-    constructor() {
-        
+    constructor() public {
+        approver = msg.sender;
     }
 
-    /* if SS at index _targetSSIndex is currently not held by another user, transfer ownership to _holder*/ 
-    function addPanelHolder(uint _targetSSIndex, address _holder) {
-        SolarSystem memory targetSS = solarSystems[_targetSSIndex];
-        require(targetSS.holdingStatus != HoldingStatus.AVAILABLE);
+    
+    function addSolarSystem(string _name, uint _pricePerKWH) public {
+        require(msg.sender == approver);
 
-        targetSS.currentHolder = _holder;
-        
-        AddHolder(_holder, _targetSSIndex);
+        SolarSystem memory newSystem = SolarSystem({
+            name: _name,
+            holdingStatus: HoldingStatus.AVAILABLE,
+            currentHolder: approver,
+            paymentStatus: PaymentStatus.PAID,
+            unpaidBalance: 0,
+            pricePerKWH: _pricePerKWH
+        });
+
+        solarSystems.push(newSystem);
+
+        emit AddSolarSystem(_name);
     }
 
-    function removePanelHolder(uint _targetSSIndex) {
-        SolarSystem memory targetSS = solarSystems[_targetSSIndex];
+    /* Transfer ownership of solar system at _targetSSIndex to _to */ 
+    function transferPanelHolder(uint _targetSSIndex, address _to) public {
+        SolarSystem storage targetSS = solarSystems[_targetSSIndex];
+        if (msg.sender != approver) {
+            require((targetSS.holdingStatus == HoldingStatus.HELD) && (targetSS.currentHolder == msg.sender));
+        }
+
+        targetSS.holdingStatus = HoldingStatus.HELD;
+        targetSS.currentHolder = _to;
+        
+        emit TransferHolder(_to, _targetSSIndex);
+    }
+
+    function removePanelHolder(uint _targetSSIndex) public {
+        SolarSystem storage targetSS = solarSystems[_targetSSIndex];
         require(targetSS.holdingStatus != HoldingStatus.HELD);
         require(targetSS.currentHolder != msg.sender);
 
@@ -48,22 +72,22 @@ contract SolarProperty {
 
     /* TODO this can only be triggered by the device on the panel */
     /* TODO do this once at the end of each day */
-    function energyProduced(uint _ssIndex, uint _kWhProduced) {
-        SolarSystem memory producingSS = solarSystems[_ssIndex];
+    function energyProduced(uint _ssIndex, uint _kWhProduced) public {
+        SolarSystem storage producingSS = solarSystems[_ssIndex];
         producingSS.unpaidBalance += _kWhProduced*producingSS.pricePerKWH;
     }
 
     /* payment by the currentHolder for the energy consumed */
-    function pay(uint _ssIndex) payable {
-        SolarSystem memory targetSS = solarSystems[_ssIndex];
+    function pay(uint _ssIndex) payable public {
+        SolarSystem storage targetSS = solarSystems[_ssIndex];
         targetSS.unpaidBalance -= msg.value;
-        Payment(targetSS.currentHolder, targetSS.unpaidBalance);
+        emit Payment(targetSS.currentHolder, targetSS.unpaidBalance);
     }
 
     /* transfer of ownership away from currentHolder if fails to pay */
-    function repo(uint _ssIndex) {
+    function repo(uint _ssIndex) public {
         address overdueHolder = solarSystems[_ssIndex].currentHolder;
-        Repo(_ssIndex, overdueHolder);
+        emit Repo(_ssIndex, overdueHolder);
         
         removePanelHolder(_ssIndex);
         
