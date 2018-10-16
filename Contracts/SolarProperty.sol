7d36fa623057f8b7835a2786337345a99ebec242
Michelle Lauer <mflauer@mit.edu>
2018-10-16 14:35:20 -0400

    preliminary confirmation code, updates to usage and energy consumption changes

--- Contracts/SolarProperty.sol
+++ Contracts/SolarProperty.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.0;
 contract SolarProperty {
+
+    uint constant PREPA_PRICE = 10; //$/kWh
     
     /* declaration of specialized data types */
     enum HoldingStatus {OWNED, HELD}
@@ -8,44 +10,102 @@ contract SolarProperty {
         uint percentageHeld; // must be maintained that the percentageHeld for all holders sums to 100
         HoldingStatus holdingStatus;
         uint lastFullPaymentTimestamp;
-        uint unpaidBalance;
+        uint unpaidUsage; // measured in kWh
     }
 
     struct SolarSystem {
-        string name;
-        uint pricePerKWH; // add any other properties of the system here
+        uint totalValue;
         mapping(address => Holder) holders;
+    }
+
+    struct ProposedDeployment {
+        uint panelSize; // insert other physical properties here
+        uint totalValue;
+        address contractor;
+        address consumer;
+        boolean isConfirmedByContractor;
     }
     
     /* public variables */
     address admin;
     mapping(address => SolarSystem) public solarSystems;
+    mapping(address => ProposedDeployment) public proposedDeployments;
 
     /* runs at initialization when contract is executed */
     constructor() public {
         admin = msg.sender;
     }
 
-    // add a new solar panel to our platform and set its name _name, physical properties (_pricePerKWH), 
+    // CONFIRMATION
+    // agreement between contractor, participant, and investor
+    function proposeDeployment(address _ssAddress, uint _panelSize, uint _totalValue, address _contractor, address _consumer) public {
+        // TODO: this should be called by a verified investor, so need some way to onboard investors/contractors
+        ProposedDeployment memory newDeployment;
+        newDeployment.panelSize = _panelSize;
+        newDeployment.totalValue = _totalValue;
+        newDeployment.contractor = _contractor;
+        newDeployment.consumer = _consumer;
+        newDeployment.isConfirmedByContractor = false;
+    }
+
+    function confirmDeployment(address _ssAddress, address confirmer) public {
+        if (proposedDeployments[_ssAddress].contractor == confirmer) {
+            isConfirmedByContractor = true;
+        } else if (isConfirmedByContractor && proposedDeployments[_ssAddress].consumer == confirmer) {
+            ProposedDeployment newDeployment = proposedDeployments[_ssAddress];
+            addSolarSystem(newDeployment.totalValue, _ssAddress);
+            payContractor();
+        }
+    }
+
+    function payContractor() {
+        //TODO ask neighborly?
+    }
+
+    
+    // This function will be called once there is confirmation of this system
+    // add a new solar panel to our platform and set its properties (_totalValue), 
     // and its uniquely identifying address on the chain
-    function addSolarSystem(string _name, uint _pricePerKWH, address _ssAddress) public {
+    function addSolarSystem(uint _totalValue, address _ssAddress) public {
         require(msg.sender == admin);
 
         Holder memory adminHolder = Holder({
             percentageHeld: 100,
             holdingStatus: HoldingStatus.HELD,
             lastFullPaymentTimestamp: now,
-            unpaidBalance: 0
+            unpaidUsage: 0
         });
         
         SolarSystem memory newSystem;
-        newSystem.name = _name;
-        newSystem.pricePerKWH = _pricePerKWH;
+        newSystem.totalValue = _totalValue;
 
         solarSystems[_ssAddress] = newSystem;
         
         // need to access holder in this way for access to storage
         solarSystems[_ssAddress].holders[admin] = adminHolder; 
+    }
+
+    // Record energy consumed by panel at targetSSAddress by consumer (called by solar panel)
+    function energyConsumed(address ssAddress, address consumer, uint energyConsumed) {
+        require((msg.sender == admin) || (msg.sender == ssAddress));
+
+        solarSystems[ssAddress].holders[consumer].unpaidUsage += energyConsumed;
+    }
+    
+
+    // Make a payment from consumer toward any unpaid balance on the panel at ssAddress
+    function makePayment(address ssAddress, address consumer, uint amountPaid) public {
+        require((msg.sender == admin) || (msg.sender == consumer));
+
+        uint amountPaidInEnergy = amountPaid/PREPA_PRICE; 
+
+        Holder storage consumerHolder = solarSystems[ssAddress].holders[consumer];
+        if (amountPaidInEnergy >= consumerHolder.unpaidUsage) {
+            consumerHolder.lastFullPaymentTimestamp = now;
+        }
+        
+        addSSHolding(amountPaid/solarSystems[ssAddress].totalValue*100, ssAddress, consumer); // transfer over a portion of ownership
+        consumerHolder.unpaidUsage -= amountPaidInEnergy; // update the unpaid balance 
     }
 
     // Transfer percentTransfer percent of holding of solar system at targetSSAddress to the user with address 'to'
@@ -55,15 +115,19 @@ contract SolarProperty {
         mapping(address => Holder) targetSSHolders = solarSystems[targetSSAddress].holders;
         require(targetSSHolders[admin].percentageHeld >= percentTransfer);
 
-        targetSSHolders[admin].percentageHeld -= percentTransfer;
         if (targetSSHolders[to].holdingStatus == HoldingStatus.HELD) {
-            targetSSHolders[to].percentageHeld += percentTransfer;
-        } else {
+            if (targetSSHolders[to].percentageHeld + percentTransfer >= 100) { // fully paid off!
+                grantOwnership(targetSSAddress, to);
+            } else {
+                targetSSHolders[to].percentageHeld += percentTransfer;
+                targetSSHolders[admin].percentageHeld -= percentTransfer;
+            }
+        } else { // this is their first payment towards this solar system
             targetSSHolders[to] = Holder({
                 percentageHeld: percentTransfer,
                 holdingStatus: HoldingStatus.HELD,
                 lastFullPaymentTimestamp: now,
-                unpaidBalance: 0
+                unpaidUsage: 0
             });
         }
     }
@@ -85,33 +149,6 @@ contract SolarProperty {
 
         mapping(address => Holder) targetSSHolders = solarSystems[targetSSAddress].holders;
         targetSSHolders[newOwner].holdingStatus = HoldingStatus.OWNED;
-    }
-
-    // Given a solar panel at ssAddress and the kWh produced, and given a consumer and their kWhConsumed, 
-    // update the balance they owe on this solar panel
-    function recordEnergyMatchupForConsumer(address ssAddress, uint kWhProduced, address consumer, uint kWhConsumed) public {
-        require(msg.sender == admin);
-
-        Holder storage consumerHolder = solarSystems[ssAddress].holders[consumer];
-        uint energyForConsumer = (kWhProduced * consumerHolder.percentageHeld)/100;
-        if (kWhConsumed >= energyForConsumer) {
-            consumerHolder.unpaidBalance += solarSystems[ssAddress].pricePerKWH * energyForConsumer;
-        } else { // didn't use all of the produced energy
-            consumerHolder.unpaidBalance += solarSystems[ssAddress].pricePerKWH * kWhConsumed;
-        }
-    }
-
-    // Make a payment from consumer toward any unpaid balance on the panel at ssAddress
-    function makePayment(address ssAddress, address consumer) payable public {
-        require((msg.sender == admin) || (msg.sender == consumer));
-
-        Holder storage consumerHolder = solarSystems[ssAddress].holders[consumer];
-        if (msg.value >= consumerHolder.unpaidBalance) {
-            consumerHolder.lastFullPaymentTimestamp = now;
-            consumerHolder.unpaidBalance = 0;
-        } else {
-            consumerHolder.unpaidBalance -= msg.value; 
-        }
     }
 
 
