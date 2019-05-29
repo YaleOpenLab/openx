33b27cd365fdc81ec940ffce32454e938c993e08
Varunram <vrg2009@ymail.com>
2019-05-29 13:26:26 -0400

    add withdraw endpoints

--- rpc/anchor.go
+++ rpc/anchor.go
@@ -10,8 +10,10 @@ import (
 )
 
 func setupAnchorHandlers() {
-	intentAnchor()
-	kycAnchor()
+	intentDeposit()
+	kycDeposit()
+	intentWithdraw()
+	kycWithdraw()
 }
 
 type AnchorIntentResponse struct {
@@ -20,7 +22,7 @@ type AnchorIntentResponse struct {
 	Identifier string `json:"identifier"`
 }
 
-type KycAnchorResponse struct {
+type kycDepositResponse struct {
 	Error  string
 	Result string
 	Url    string
@@ -34,7 +36,7 @@ func PostAndSend(w http.ResponseWriter, r *http.Request, body string, payload io
 		responseHandler(w, StatusBadRequest)
 		return
 	}
-	var x KycAnchorResponse
+	var x kycDepositResponse
 	err = json.Unmarshal(data, &x)
 	if err != nil {
 		log.Println("did not unmarshal json", err)
@@ -44,9 +46,9 @@ func PostAndSend(w http.ResponseWriter, r *http.Request, body string, payload io
 	MarshalSend(w, x)
 }
 
-func intentAnchor() {
+func intentDeposit() {
 	// curl 'https://sandbox-api.anchorusd.com/transfer/deposit?account=GBP3XOFYC6TWUIRZAB7MB6MTUZBCREAYB4E7XKE3OWDP75VU5JB74ZF6&asset_code=USD&email_address=j%40anchorusd.com
-	http.HandleFunc("/user/anchorusd/intent", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/user/anchorusd/deposit/intent", func(w http.ResponseWriter, r *http.Request) {
 		checkGet(w, r)
 		checkOrigin(w, r)
 
@@ -59,12 +61,12 @@ func intentAnchor() {
 		body := "https://sandbox-api.anchorusd.com/transfer/deposit?account=" + prepUser.PublicKey +
 			"&asset_code=USD&email_address=" + prepUser.Email
 		var x AnchorIntentResponse
-		GetAndSendJson(w, r, body, x)
+		GetAndSendJson(w, r, body, x) // we could return the identifier and save it if we have to. But the user has to click through anyawy and we could call the other endpoint from the frontend, so would need to discuss before we do that here
 	})
 }
 
-func kycAnchor() {
-	http.HandleFunc("/user/anchorusd/kyc", func(w http.ResponseWriter, r *http.Request) {
+func kycDeposit() {
+	http.HandleFunc("/user/anchorusd/deposit/kyc", func(w http.ResponseWriter, r *http.Request) {
 		checkGet(w, r)
 		checkOrigin(w, r)
 
@@ -77,6 +79,60 @@ func kycAnchor() {
 		body := "https://sandbox-api.anchorusd.com/api/register"
 		data := url.Values{}
 		data.Set("identifier", "b5836f518685c372fe5a013475ccbf37")
+		data.Set("name", "Test User")
+		data.Set("birthday[month]", "6")
+		data.Set("birthday[day]", "8")
+		data.Set("birthday[year]", "1993")
+		data.Set("tax-country", "US")
+		data.Set("tax-id-number", "111111111")
+		data.Set("address[street-1]", "123 4 Street")
+		data.Set("address[city]", "Anytown")
+		data.Set("address[postal-code]", "94107")
+		data.Set("address[region]", "CA")
+		data.Set("address[country]", "US")
+		data.Set("primary-phone-number", "+14151111111")
+		data.Set("gender", "male")
+
+		payload := strings.NewReader(data.Encode())
+		PostAndSend(w, r, body, payload)
+	})
+}
+
+func intentWithdraw() {
+	// curl 'https://sandbox-api.anchorusd.com/transfer/withdraw?type=bank_account&asset_code=USD&email_address=j%40anchorusd.com
+	http.HandleFunc("/user/anchorusd/withdraw/intent", func(w http.ResponseWriter, r *http.Request) {
+		// the withdraw endpoint doesn't return an identifier and we'd have to parse some stuff ourselves. Ugly hack and we shouldn't really ahve to do this, should be fixed by Anchor
+		checkGet(w, r)
+		checkOrigin(w, r)
+
+		prepUser, err := UserValidateHelper(w, r)
+		if err != nil {
+			responseHandler(w, StatusUnauthorized)
+			return
+		}
+
+		// amount can be chosen by the user in the flow on anchor, so no need to handle that here
+		body := "https://sandbox-api.anchorusd.com/transfer/withdraw?type=bank_account&asset_code=USD&account=" + prepUser.PublicKey +
+			"&email_address=" + prepUser.Email
+		var x AnchorIntentResponse
+		GetAndSendJson(w, r, body, x)
+	})
+}
+
+func kycWithdraw() {
+	http.HandleFunc("/user/anchorusd/withdraw/kyc", func(w http.ResponseWriter, r *http.Request) {
+		checkGet(w, r)
+		checkOrigin(w, r)
+
+		_, err := UserValidateHelper(w, r)
+		if err != nil {
+			responseHandler(w, StatusUnauthorized)
+			return
+		}
+
+		body := "https://sandbox-api.anchorusd.com/api/register"
+		data := url.Values{}
+		data.Set("identifier", "54071eb5789afa0af61d126559dcd6a6") // TODO: the deposit API doesn't parse identifiers. Should be fixed on AnchorUSD's end.
 		data.Set("name", "Test User")
 		data.Set("birthday[month]", "6")
 		data.Set("birthday[day]", "8")
