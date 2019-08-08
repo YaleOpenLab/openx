33edd640e69db6303f0a4cee9396a085fad14758
Varunram <vrg2009@ymail.com>
2019-08-08 14:41:55 +0530

    being rpc handlers specific to platforms

--- rpc/platforms.go
+++ rpc/platforms.go
@@ -5,11 +5,16 @@ import (
 	"net/http"
 
 	erpc "github.com/Varunram/essentials/rpc"
+	utils "github.com/Varunram/essentials/utils"
 	consts "github.com/YaleOpenLab/openx/consts"
+	database "github.com/YaleOpenLab/openx/database"
 )
 
 func setupPlatformRoutes() {
-	getConsts()
+	pfGetConsts()
+	pfGetUser()
+	pfValidateUser()
+	pfNewUser()
 }
 
 type OpensolarConstReturn struct {
@@ -27,7 +32,7 @@ type OpensolarConstReturn struct {
 	DbDir               string
 }
 
-func getConsts() {
+func pfGetConsts() {
 	http.HandleFunc("/platform/getconsts", func(w http.ResponseWriter, r *http.Request) {
 		erpc.CheckGet(w, r)
 		erpc.CheckOrigin(w, r)
@@ -57,5 +62,82 @@ func getConsts() {
 			erpc.MarshalSend(w, x)
 			return
 		}
+	})
+}
+
+func pfGetUser() {
+	http.HandleFunc("/platform/user/retrieve", func(w http.ResponseWriter, r *http.Request) {
+		erpc.CheckGet(w, r)
+		erpc.CheckOrigin(w, r)
+
+		if r.URL.Query()["code"] == nil || r.URL.Query()["key"] == nil {
+			log.Println("code missing")
+			erpc.ResponseHandler(w, erpc.StatusBadRequest)
+		}
+
+		code := r.URL.Query()["code"][0]
+
+		if code == "OPENSOLARTEST" {
+			keyInt, err := utils.ToInt(r.URL.Query()["key"][0])
+			if err != nil {
+				erpc.ResponseHandler(w, erpc.StatusBadRequest)
+				return
+			}
+			user, err := database.RetrieveUser(keyInt)
+			if err != nil {
+				erpc.ResponseHandler(w, erpc.StatusBadRequest)
+				return
+			}
+			erpc.MarshalSend(w, user)
+		}
+	})
+}
+
+func pfValidateUser() {
+	http.HandleFunc("/platform/user/validate", func(w http.ResponseWriter, r *http.Request) {
+		erpc.CheckGet(w, r)
+		erpc.CheckOrigin(w, r)
+
+		if r.URL.Query()["name"] == nil || r.URL.Query()["pwhash"] == nil {
+			log.Println("code missing")
+			erpc.ResponseHandler(w, erpc.StatusBadRequest)
+		}
+
+		name := r.URL.Query()["name"][0]
+		pwhash := r.URL.Query()["pwhash"][0]
+
+		user, err := database.ValidateUser(name, pwhash)
+		if err != nil {
+			erpc.ResponseHandler(w, erpc.StatusBadRequest)
+			return
+		}
+		erpc.MarshalSend(w, user)
+	})
+}
+
+func pfNewUser() {
+	http.HandleFunc("/platform/user/new", func(w http.ResponseWriter, r *http.Request) {
+		erpc.CheckGet(w, r)
+		erpc.CheckOrigin(w, r)
+
+		if r.URL.Query()["name"] == nil || r.URL.Query()["pwhash"] == nil || r.URL.Query()["seedpwd"] == nil ||
+			r.URL.Query()["realname"] == nil {
+			log.Println("code missing")
+			erpc.ResponseHandler(w, erpc.StatusBadRequest)
+		}
+
+		name := r.URL.Query()["name"][0]
+		pwhash := r.URL.Query()["pwhash"][0]
+		seedpwd := r.URL.Query()["seedpwd"][0]
+		realname := r.URL.Query()["realname"][0]
+
+		user, err := database.NewUser(name, pwhash, seedpwd, realname)
+		if err != nil {
+			erpc.ResponseHandler(w, erpc.StatusBadRequest)
+			return
+		}
+
+		log.Println("USER+", user)
+		erpc.MarshalSend(w, user)
 	})
 }
