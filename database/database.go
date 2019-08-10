8a4cacab146cf11801f3443da68f78c93021e964
Varunram <vrg2009@ymail.com>
2019-08-10 11:27:17 +0530

    add comments for database

--- database/database.go
+++ database/database.go
@@ -6,21 +6,24 @@ import (
 	"github.com/boltdb/bolt"
 )
 
+// the database package contains the handlers necesssary for openx to interact with the
+// underlying boltdb database
+
 var UserBucket = []byte("Users")
 
-// CreateHomeDir creates a home directory
+// CreateHomeDir creates the home and database directories
 func CreateHomeDir() {
 	edb.CreateDirs(consts.HomeDir, consts.DbDir)
 	db, _ := edb.CreateDB(consts.DbDir+consts.DbName, UserBucket)
 	db.Close()
 }
 
-// OpenDB opens the db
+// OpenDB opens the db and returns a pointer to the database
 func OpenDB() (*bolt.DB, error) {
 	return edb.OpenDB(consts.DbDir + consts.DbName)
 }
 
-// DeleteKeyFromBucket deletes a given key from the bucket
+// DeleteKeyFromBucket deletes an object from the passed bucket
 func DeleteKeyFromBucket(key int, bucketName []byte) error {
 	return edb.DeleteKeyFromBucket(consts.DbDir+consts.DbName, key, bucketName)
 }
