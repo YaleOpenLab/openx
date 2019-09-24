da06aa945117c8e4682bc27d81e051137616c775
Varunram <varunramganesh@gmail.com>
2019-09-24 16:33:21 +0530

    temp ci commit

---
+++ ci/ci.sh
@@ -0,0 +1,22 @@
+echo $GOPATH
+if [ "$GOPATH" == "" ] ; then
+  WGO="$(which go)"
+  if [ "$WGO" == "" ] ; then
+    echo "installing golang on your machine"
+    sudo apt-get -y upgrade
+    wget https://dl.google.com/go/go1.12.4.linux-amd64.tar.gz
+    sudo tar -xvf go1.12.4.linux-amd64.tar.gz
+    sudo mv go /usr/local
+    echo 'GOROOT=/usr/local/go' >> ~/.profile
+    echo 'GOPATH=$HOME' >> ~/.profile
+    echo 'PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.profile
+    source ~/.profile
+  fi
+fi
+
+# At this instance, we assume golang is installed on the user's machine
+go get -v github.com/YaleOpenLab/openx
+cd $GOPATH/go/src/github.com/YaleOpenLab/openx
+go get -v ./...
+go build -v ./...
+cp openx teller/teller ~/
