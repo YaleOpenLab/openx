3d246b0f563937e63a0591b3cd1c15e26ba8bbce
Varunram <varunramganesh@gmail.com>
2019-09-26 14:45:56 +0530

    rename

--- ci/build.sh
+++ ci/build.sh
@@ -75,7 +75,7 @@ mv openx.gz $GOPATH/go/src/github.com/YaleOpenLab/openx/ci
 
 cd $GOPATH/go/src/github.com/YaleOpenLab/
 cp -r opensolar opensolar-temp
-cd openx-temp
+cd opensolar-temp
 rm -rf .git/ ci/
 cd ..
 tar -cvzf opensolar.gz opensolar-temp/
